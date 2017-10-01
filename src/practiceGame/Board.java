1d8731d13823e587f0b030793bdcf7cb7a03e0ea
Kyle <kderrick1988@gmail.com>
2017-10-01 14:34:12 -0700

    draw image on Board

--- src/practiceGame/Board.java
+++ src/practiceGame/Board.java
@@ -1,8 +1,49 @@
 package practiceGame;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Ellipse2D;
+
+import javax.swing.JPanel;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 public class Board extends JPanel {
 
-    public Board() {}
+    private Image bardejov;
+
+    public Board() {
+
+        initBoard();
+    }
+    
+    private void initBoard() {
+        
+        loadImage();
+        
+        int w = bardejov.getWidth(this);
+        int h =  bardejov.getHeight(this);
+        setPreferredSize(new Dimension(w, h));        
+    }
+    
+    private void loadImage() {
+        
+        ImageIcon ii = new ImageIcon("src/images/bardejov.png");
+        bardejov = ii.getImage(); 
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+
+        g.drawImage(bardejov, 0, 0, null);
+    }
 }
